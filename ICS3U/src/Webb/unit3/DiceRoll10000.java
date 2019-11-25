2edcef35ddff5273f8ef9159cb9d5dcb080b922f
334750643 <334750643@C-DNY5WR1.tdsb.on.ca>
2019-11-25 13:43:45 -0500

    finishing on nov24

---
+++ ICS3U/src/Webb/unit3/DiceRoll10000.java
@@ -0,0 +1,10 @@
+package Webb.unit3;
+
+public class DiceRoll10000 {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
--- ICS3U/src/Webb/unit3/HappySong.java
+++ ICS3U/src/Webb/unit3/HappySong.java
@@ -4,12 +4,17 @@ public class HappySong {
 
 	public static void main(String[] args) {
 		
-		String[] lyrics = {"and you really want to show it","clap your hands"," stomp your feet", 
-				" write some code", "say hello"," raise your hand", "sing a song", " spin around",
-				" jump up and down", " step to the left", " step to the right"};
+		String[] lyrics = {"clap your hands","stomp your feet", "write some code", "say hello",
+				"raise your hand", "sing a song", "spin around", "jump up and down",
+				"step to the left", "step to the right", "and you really want to show it"};
 		for (int i = 0; i<10; i++)
 		{
-			system
+			System.out.println("if your happy and you know it "+lyrics[i]);
+			System.out.println("if your happy and you know it "+lyrics[i]);
+			System.out.println("if your happy and you know it "+lyrics[10]);
+			System.out.println("if your happy and you know it "+lyrics[i]);
+			System.out.println(" ");
+			
 		}
 	}
 
