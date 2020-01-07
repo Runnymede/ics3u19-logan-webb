a3e452a54ce4c57a9773bd9b17dd0bdcb0db8e3f
334750643 <334750643@C-DNY5WR1.tdsb.on.ca>
2020-01-07 13:37:21 -0500

    work for jan 7 part 3

--- ICS3U/src/Webb/unit4/MathPlus.java
+++ ICS3U/src/Webb/unit4/MathPlus.java
@@ -17,10 +17,10 @@ public class MathPlus {
 		String calculate = sc.next();
 
 		if (calculate.equals("max")){
-			System.out.println(max(numbers,amountofnumbers));
+			System.out.println(numbers[min(numbers,amountofnumbers)]);
 		}
 		else if (calculate.equals("min")){
-			System.out.println(min(numbers,amountofnumbers));
+			System.out.println(numbers[min(numbers,amountofnumbers)]);
 		}
 		else if (calculate.equals("sum")){
 			System.out.println(sum(numbers,amountofnumbers));
@@ -30,6 +30,13 @@ public class MathPlus {
 		}
 
 	}
+	/**
+	 * method name - min
+	 * this method returns the index of the first occurrence of the smallest number in the array
+	 * @param numbers (int[])
+	 * @param amountofnumbers (int)
+	 * @return minindex (int)
+	 */
 	public static int min (int[] numbers, int amountofnumbers){
 		int min=numbers[0];
 		int minindex = 0;
@@ -44,6 +51,13 @@ public class MathPlus {
 		}
 		return minindex;
 	}
+	/**
+	 *  method name - max
+	 * this method returns the index of the first occurrence of the largest number in the array
+	 * @param numbers (int[])
+	 * @param amountofnumbers (int)
+	 * @return maxindex (int)
+	 */
 	public static int max (int[] numbers, int amountofnumbers){
 		
 		int max=numbers[0];
@@ -59,6 +73,12 @@ public class MathPlus {
 		}
 		return maxindex;
 	}
+	/**
+	 * this method returns the sum of all the integers in the given array
+	 * @param numbers (int[])
+	 * @param amountofnumbers (int)
+	 * @return sum (int)
+	 */
 	public static int sum (int[] numbers, int amountofnumbers){
 		int sum = 0;
 		int total = 0;
@@ -69,9 +89,97 @@ public class MathPlus {
 		}	
 		return sum;
 	}
+	/**
+	 * this method returns the average of all the integers in the given array
+	 * @param numbers (int)
+	 * @param amountofnumbers (int)
+	 * @return average (double)
+	 */
 	public static double average (int[] numbers, int amountofnumbers){
 		int sum = 0;
 		int total = 0;
+		for(int i = 0; i<amountofnumbers; i++) {
+
+			total=numbers[i]+ total;
+			sum=total;
+		}
+		sum /= amountofnumbers;
+		return sum;
+	}
+	
+	
+	
+	// all doubles
+	
+	
+	/**
+	 * method name - min
+	 * this method returns the index of the first occurrence of the smallest number in the array
+	 * @param numbers (double[])
+	 * @param amountofnumbers (double)
+	 * @return minindex (double)
+	 */
+	public static double min (double[] numbers, double amountofnumbers){
+		double min=numbers[0];
+		int minindex = 0;
+		
+		for(int i = 0; i<amountofnumbers; i++) {
+
+			if (numbers[i]>min)
+			{
+				min=numbers[i];
+				minindex = i;
+			}
+		}
+		return minindex;
+	}
+	/**
+	 *  method name - max
+	 * this method returns the index of the first occurrence of the largest number in the array
+	 * @param numbers (double[])
+	 * @param amountofnumbers (double)
+	 * @return maxindex (double)
+	 */
+	public static double max (double[] numbers, double amountofnumbers){
+		
+		double max=numbers[0];
+		int maxindex = 0;
+		
+		for(int i = 0; i<amountofnumbers; i++) {
+
+			if (numbers[i]>max)
+			{
+				max=numbers[i];
+				maxindex = i;
+			}
+		}
+		return maxindex;
+	}
+	/**
+	 * this method returns the sum of all the integers in the given array
+	 * @param numbers (double[])
+	 * @param amountofnumbers (double)
+	 * @return sum (double)
+	 */
+	public static double sum (double[] numbers, double amountofnumbers){
+		double sum = 0;
+		double total = 0;
+		for(int i = 0; i<amountofnumbers; i++) {
+
+			total=numbers[i]+ total;
+			sum=total;
+		}	
+		return sum;
+	}
+	/**
+	 * this method returns the average of all the integers in the given array
+	 * @param numbers (double)
+	 * @param amountofnumbers (double)
+	 * @return average (double)
+	 */
+	public static double average (double[] numbers, double amountofnumbers){
+		double sum = 0;
+		double total = 0;
 		for(int i = 0; i<amountofnumbers; i++) {
 
 			total=numbers[i]+ total;
